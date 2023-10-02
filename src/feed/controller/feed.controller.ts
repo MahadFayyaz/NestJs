ebc285868bd958dca48ca09cb9528b3576884b45
MahadFayyaz <fayazmahad675@gmail.com>
2023-10-02 17:10:38 +0500

    Latest update

--- src/feed/controller/feed.controller.ts
+++ src/feed/controller/feed.controller.ts
@@ -6,22 +6,29 @@ import {
   Put,
   Param,
   Delete,
+  HttpException,
+  HttpStatus
 } from '@nestjs/common';
 import { FeedService } from '../services/feed.service';
 import { FeedPost } from '../models/post.interface';
 import { Observable } from 'rxjs';
 import { DeleteResult, UpdateResult } from 'typeorm';
+import { UserDetailsEntity } from 'src/user/entities/user.entity';
+import { FeedPostEntity } from '../models/post.entity';
 
 @Controller('feed')
 export class FeedController {
   constructor(private feedService: FeedService) {}
   @Post('post')
-  create(@Body() post: FeedPost): Observable<FeedPost> {
-    return this.feedService.createPost(post);
+  async create(@Body() postData: FeedPostEntity, @Body('createdBy') createdBy: number): Promise<FeedPostEntity> {
+    if (!postData.body || !createdBy) {
+        throw new HttpException('Both body and createdBy are required', HttpStatus.BAD_REQUEST);
+      }
+    return await this.feedService.createPost(postData, createdBy);  
   }
   @Get('getAll')
   findAll(): Observable<FeedPost[]> {
-    return this.feedService.findAllPosts();
+    return this.feedService.findAllPosts(); 
   }
 
   @Put(':id')
--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -1,33 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import {DeleteResult, Repository, UpdateResult} from 'typeorm';
-
 import {InjectRepository} from '@nestjs/typeorm'
 import { FeedPostEntity } from '../models/post.entity';
 import { FeedPost } from '../models/post.interface'; 
 import { Observable, from } from 'rxjs';
+import { UserDetailsEntity } from 'src/user/entities/user.entity';
 // import { FeedPost } from '../models/post.interface';
 
 @Injectable()
 export class FeedService {
     constructor(
         @InjectRepository(FeedPostEntity)
-        private readonly feedPostRepository : Repository<FeedPostEntity>
-    ) { }
-    createPost(feedPost: FeedPost): Observable<FeedPost> {
-        return from(this.feedPostRepository.save(feedPost));
-    }
+    private readonly feedPostRepository: Repository<FeedPostEntity>,
+    @InjectRepository(UserDetailsEntity) // Inject UserDetailsEntity repository
+    private readonly userRepository: Repository<UserDetailsEntity>, // Add this line
+  ) {}
+
+    async createPost(feedPost: FeedPostEntity, createdBy: number): Promise<FeedPostEntity> {
+        const user = await this.userRepository.findOne({ where: { id: createdBy}});
+        if (!user) {
+          throw new HttpException('Invalid user ID', HttpStatus.BAD_REQUEST);
+        }
+    
+        feedPost.createdBy = user;
+        return await this.feedPostRepository.save(feedPost);
+      }
 
     findAllPosts() : Observable<FeedPost[]> {
-        return from(this.feedPostRepository.find().then(posts => {
+        return from(this.feedPostRepository.find({
+            relations: ['createdBy'] // Load the createdBy relationship
+          }).then(posts => {
             return posts.map(post => {
                 return {
                     id:post.id,
                     body: post.body.toString(),
                     createdAt: post.createdAt,
-                };
+                    createdBy:post.createdBy,
+             };
             });
         }));
-    }
+    }   
     updatePost(id:number, feedPost: FeedPost): Observable<UpdateResult> {
         return from(this.feedPostRepository.update(id,feedPost))
     }
