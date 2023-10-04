949ee4f9765faabdc34f7a8342f1b3a676807053
MahadFayyaz <fayazmahad675@gmail.com>
2023-10-04 11:33:23 +0500

    jwt token

---
+++ src/user/entities/jwt-payload.interface.ts
@@ -0,0 +1,4 @@
+export interface JwtPayload {
+    id:number;
+    email: string; // Add any additional information you want to include in the token
+  }
---
+++ src/user/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './entities/jwt-payload.interface';
+import { UserService } from './user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy)
+{
+    constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'this is a jwt key',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.userService.findById(payload.id);
+
+        if (!user) {
+        throw new UnauthorizedException();
+        }
+
+    return user;
+  }
+}
